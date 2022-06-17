ef1ccd028fa4aaa38f805e855fdb1832b3e139b3
MarcelloMuy <marcellomuylaert@icloud.com>
2022-06-17 18:16:37 +0000

    Added navbar, footer and carousel to home page

---
+++ static/js/base.js
@@ -0,0 +1,8 @@
+
+$("#carousel-main-1").delay(1500).animate({"opacity": "1"}, 700);
+
+$("#carousel-main-2").delay(2500).animate({"opacity": "1"}, 700);
+
+$("#shop-now-btn").delay(3500).animate({"opacity": "1"}, 700);
+
+$('.carousel').carousel({interval: 6000});
