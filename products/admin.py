b02dc62e9d018b6e9ff188d09d3dab132baf52d0
MarcelloMuy <marcellomuylaert@icloud.com>
2022-06-26 13:21:55 +0000

    Added new products, Customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    list_filter = ('category',)
+
+    ordering = ('sku',)
+
+    search_fields = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
