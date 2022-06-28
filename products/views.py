37fd527bd053e13cc5603bdcc81a8ae4c0ee22fe
MarcelloMuy <marcellomuylaert@icloud.com>
2022-06-28 19:06:28 +0000

    Added sorting functionality

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
 # Imported models
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from. models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -10,8 +10,29 @@ def all_products(request):
 
     products = Product.objects.order_by("name")
     query = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)    
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,9 +42,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
