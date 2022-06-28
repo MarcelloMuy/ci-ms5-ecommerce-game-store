37d7863d11ac39c830b3623d01427375300dba04
MarcelloMuy <marcellomuylaert@icloud.com>
2022-06-28 18:26:08 +0000

    Added searh functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 # Imported models
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from. models import Product
 
 
@@ -7,9 +9,21 @@ def all_products(request):
     """ Display all products including search and filtering queries """
 
     products = Product.objects.order_by("name")
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
