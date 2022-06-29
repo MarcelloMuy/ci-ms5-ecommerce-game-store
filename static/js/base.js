f1644e465f8ef5a88bdfee92400a9c9f44ab740d
MarcelloMuy <marcellomuylaert@icloud.com>
2022-06-29 21:07:59 +0000

    Added add to bag functionality

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,10 @@
 
 // Animate contents of carousel
 
-$("#carousel-main-1").delay(1500).animate({"opacity": "1"}, 700);
+$("#carousel-main-1").delay(1250).animate({"opacity": "1"}, 700);
 
-$("#carousel-main-2").delay(2500).animate({"opacity": "1"}, 700);
+$("#carousel-main-2").delay(2250).animate({"opacity": "1"}, 700);
 
-$("#shop-now-btn").delay(3500).animate({"opacity": "1"}, 700);
+$("#shop-now-btn").delay(3250).animate({"opacity": "1"}, 700);
 
 $('.carousel').carousel({interval: 10000});
