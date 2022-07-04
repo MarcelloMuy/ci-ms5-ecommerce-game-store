e50233a0112015035073d4056b9fb6ad372b71bf
MarcelloMuy <marcellomuylaert@icloud.com>
2022-07-04 20:16:07 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KxcNBLn3gXtxH4XHzmWFo0eMs8NcYAoASTHNHr3APrcVDcad6ZThx2LgfU9SNv11XUhCNWghhMQUWxKoJHdTgMD00BTAsOIyi',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
