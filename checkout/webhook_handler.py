2315cd3508ea15d1f393fbfd9b512e622322de8c
MarcelloMuy <marcellomuylaert@icloud.com>
2022-07-08 21:53:50 +0000

    Update webhook handler for profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
-
-from .models import Order, OrderLineItem
-from products.models import Product
-
+"""Imported"""
 import json
 import time
+from django.http import HttpResponse
+
+from products.models import Product
+from profiles.models import UserProfile
+
+from .models import Order, OrderLineItem
+
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -38,6 +41,25 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = (
+                    shipping_details.address.line1
+                    )
+                profile.default_street_address2 = (
+                    shipping_details.address.line2
+                    )
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -70,6 +92,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -83,14 +106,14 @@ class StripeWH_Handler:
                 )
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
-                    
+
                     order_line_item = OrderLineItem(
                         order=order,
                         product=product,
                         quantity=item_data,
                     )
                     order_line_item.save()
-                   
+
             except Exception as e:
                 if order:
                     order.delete()
