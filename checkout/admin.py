f45b4057a8dcd7de2204feab90d6276e2ed28795
MarcelloMuy <marcellomuylaert@icloud.com>
2022-07-08 21:36:31 +0000

    Added order history and update checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,9 +14,9 @@ class OrderAdmin(admin.ModelAdmin):
                        'delivery_cost', 'order_total',
                        'grand_total', 'original_bag', 'stripe_pid',)
 
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'street_address1',
-              'street_address2', 'town_or_city', 'postcode', 
+              'street_address2', 'town_or_city', 'postcode',
               'county', 'country', 'delivery_cost',
               'order_total', 'grand_total', 'original_bag', 'stripe_pid',)
 
