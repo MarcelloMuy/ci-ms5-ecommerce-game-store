e40259209b006700dbd08fca3d76e2318074d6cc
MarcelloMuy <marcellomuylaert@icloud.com>
2022-07-08 05:39:39 +0000

    Tested profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
 """Imported"""
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
     """ Display the user's profile. """
+    myprofile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': myprofile,
+    }
 
     return render(request, template, context)
