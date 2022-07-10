65f7ac6abaf186c6ada1c822352a15797a202d3e
MarcelloMuy <marcellomuylaert@icloud.com>
2022-07-10 20:38:14 +0000

    Removed secret key and set debug

--- game_store/settings.py
+++ game_store/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5+n32%ogef54zi#hmcllu3u5k0=-$n0+dgx42y$6==66oqmsso'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['marcellomuy-checkmate.herokuapp.com', 'localhost']
 
