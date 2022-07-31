a5dcf1b2102e2fa61cede3730db4abfd481b908e
MarcelloMuy <marcellomuylaert@icloud.com>
2022-07-31 17:15:14 +0000

    Update newsletter form to show boolean field and set initial value to false. Added placeholder to email field

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,22 +1,33 @@
+"Imported"
 from django import forms
-from .models import Subscriber
 from crispy_forms.helper import FormHelper
+from .models import Subscriber
 
 
 class SubscriberForm(forms.ModelForm):
+    """A class for newsletter form"""
 
-    sub_form = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+    sub_form = forms.BooleanField(initial=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
+        # Remove form label
         self.fields['email'].label = False
+        self.fields['sub_form'].label = (
+            "Please check this box if you want to continue")
+
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={'placeholder': 'Email Address'})
+        )
 
     class Meta:
+        """Meta for email field"""
         model = Subscriber
         fields = ["email"]
 
         def clean_email(self):
+            """Gets valid data only"""
             email = self.cleaned_data.get("email")
 
             return email
