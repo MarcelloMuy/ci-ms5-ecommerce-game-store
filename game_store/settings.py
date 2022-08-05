4abadfc9ade9a862dc061e914b1a8e93e099c190
MarcelloMuy <marcellomuylaert@icloud.com>
2022-08-05 18:58:20 +0000

    Removed debug=true from settings

--- game_store/settings.py
+++ game_store/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'not_empty')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['marcellomuy-checkmate.herokuapp.com', 'localhost']
 
